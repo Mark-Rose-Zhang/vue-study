d218b1267e0c037efcea1d6e85d31916166b8861
zhangjinghao03 <zhangjinghao03@meituan.com>
2023-03-29 18:24:18 +0800

    complete stage-2

--- stage-2/hello/src/App.vue
+++ stage-2/hello/src/App.vue
@@ -1,40 +1,69 @@
 <template>
   <div>
-    <Banner />
-    <div class="row">
-      <div class="col-xs-2 col-xs-offset-2">
-        <div class="list-group">
-          <!-- 原始html中我们使用a标签实现页面的跳转 -->
-          <!-- <a class="list-group-item active" href="./about.html">About</a> -->
-          <!-- <a class="list-group-item" href="./home.html">Home</a> -->
+    <el-steps :active="step" align-center style="margin-top: 20px">
+      <el-step title="步骤1" description="损坏信息填写"></el-step>
+      <el-step title="步骤2" description="损坏信息一级审批"></el-step>
+      <el-step title="步骤3" description="损坏信息二级审批"></el-step>
+      <el-step title="步骤4" description="审批结果"></el-step>
+    </el-steps>
+    <el-divider></el-divider>
 
-          <!-- Vue中借助router-link标签实现路由的切换 -->
-          <router-link class="list-group-item" active-class="active" to="/about"
-            >About</router-link
-          >
-          <router-link class="list-group-item" active-class="active" to="/home"
-            >Home</router-link
-          >
-        </div>
-      </div>
-      <div class="col-xs-6">
-        <div class="panel">
-          <div class="panel-body">
-            <!-- 指定组件的呈现位置 -->
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
+    <el-switch
+      v-model="v1"
+      v-show="step === 1"
+      active-text="按月付费"
+      inactive-text="按年付费"
+    >
+    </el-switch>
+    <el-time-select
+      v-model="t1"
+      v-show="step === 2"
+      :picker-options="{
+        start: '08:30',
+        step: '00:15',
+        end: '18:30',
+      }"
+      placeholder="选择时间"
+    >
+    </el-time-select>
+
+    <el-row>
+      <el-button
+        type="warning"
+        icon="el-icon-star-off"
+        circle
+        @click="forward"
+      ></el-button>
+      <el-button
+        type="danger"
+        icon="el-icon-delete"
+        circle
+        @click="back"
+      ></el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
-import Banner from "./components/Banner";
 export default {
   name: "App",
-  components: {
-    Banner,
+  data() {
+    return {
+      step: 1,
+      v1: true,
+      t1: "",
+    };
+  },
+  methods: {
+    forward() {
+      this.step = (this.step + 1) % 4;
+    },
+
+    back() {
+      this.step = (this.step - 1) % 4;
+    },
   },
 };
 </script>
+<style scoped>
+</style>
--- stage-2/hello/src/main.js
+++ stage-2/hello/src/main.js
@@ -2,19 +2,18 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-//引入VueRouter
-import VueRouter from 'vue-router'
-//引入路由器
-import router from './router'
 
+// 引入 Element-UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-//应用插件
-Vue.use(VueRouter)
+
+// 使用element-ui
+Vue.use(ElementUI)
 
 //创建vm
 new Vue({
 	el:'#app',
 	render: h => h(App),
-	router:router
 })
